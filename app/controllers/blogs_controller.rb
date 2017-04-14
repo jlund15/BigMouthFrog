1aeebf170f6084be267d0dee7fbe352494736dd9
Jared Lund <jared.lund@icloud.com>
2017-04-14 15:30:16 -0600

    Implemented edit and update functionality

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -29,10 +29,7 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
-      else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -46,7 +43,6 @@ class BlogsController < ApplicationController
         format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -57,7 +53,6 @@ class BlogsController < ApplicationController
     @blog.destroy
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
