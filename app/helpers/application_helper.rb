830abb7f58d90187cb278cdd9fdcf7c69ead7ab5
Jared Lund <jared.lund@icloud.com>
2017-04-24 14:26:05 -0600

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting") 
     end
   end
+  
+  def copyright_generator
+    BMFViewTool::Renderer.copyright 'Big Mouth Frog Development', 'All rights reserved'
+  end
 end
