4923319c5a9a983bf8144a8dbddde51c9bc73ba2
Jared Lund <jared.lund@icloud.com>
2017-05-08 15:11:54 -0600

    edited keys

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ Bundler.require(*Rails.groups)
 module BigMouthFrog
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
-    config.secret_key_base = ENV["SECRET_KEY-BASE"]
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
